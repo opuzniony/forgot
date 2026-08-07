1febe0ecf5c11db3db43406fbeec0b881552563d
opuzniony <padeksyt@gmail.com>
2026-08-08 00:14:06 +0200

    Added the actual python code (maybe holds together?)

---
+++ README.md
@@ -0,0 +1,8 @@
+A tool that you can save your often used commands so they don't get lost after running other ones
+
+# Command List:
+    **v** - Print version
+    **save [name] [rest of string gets saved as command]** - Saves a command inside data.json in .local/bin
+    **list** - Lists all of the saved commands (does not say their contents)
+    **rm [name]** - Removes a command
+    **run [name]** - Runs a command using os.system()
--- main.py
+++ main.py
@@ -1,14 +1,74 @@
-import json
+#!/usr/bin/env python3
+
 import os
+import sys
+import json
 
-useless = open('info.json')
-info = json.load(useless)
+home_dir = os.path.dirname(os.path.abspath(__file__))
 
-def main():
-    print(f"{info["name"]}, {info["version"]}")
-    info["name"] = "Forgor"
-    print(f"{info["name"]}, {info["version"]}")
-    useless = json.dumps(info)
-    print(useless)
-    json.dump(useless, "info.json")
-main()
+with open(os.path.join(home_dir, 'info.json'), 'r') as file:
+    info = json.load(file)
+
+args = sys.argv[1:]
+
+match args:
+    case ["v"]:
+        print(f"{info['name']} {info['version']}")
+
+    case ["save", string_name, *string]:
+
+        #directory cant be different if terminal is ran from somewhere else
+        data_path = os.path.join(home_dir, "data.json")
+
+        #see if it exists and take it
+        if os.path.exists(data_path):
+            with open(data_path, "r") as file:
+                data = json.load(file)
+
+        #fallback if theres nothing
+        else:
+            data = {"commands": {}}
+
+        #add tge stuff
+        data["commands"][string_name] = string
+
+        #write to the file with the stuff
+        with open(data_path, "w") as file:
+                json.dump(data, file, indent=4)
+
+        #confirm it so the user doesnt panick
+        print(f"Saved command as {string_name}")
+
+    case ["rm", string_name]:
+        #same as above it has to read them to edit anything
+        data_path = os.path.join(home_dir, "data.json")
+        with open(data_path, "r") as file:
+                data = json.load(file)
+
+        #check if its there and delete if yes
+        if string_name in data["commands"]:
+            del data["commands"][string_name]
+
+            with open(data_path, "w") as file:
+                json.dump(data, file, indent=4)
+
+            print(f"Deleted {string_name}")
+        else:
+            print(f"{string_name} does not exist.")
+    
+    case ["list"]:
+        data_path = os.path.join(home_dir, "data.json")
+        with open(data_path, "r") as file:
+                data = json.load(file)
+        
+        for name in data["commands"]:
+            print(name)
+    case ["run", string_name]:
+        data_path = os.path.join(home_dir, "data.json")
+        with open(data_path, "r") as file:
+                data = json.load(file)
+        
+        command = " ".join(data["commands"][string_name])
+        os.system(command)
+    case _:
+        print("Unknown command")
